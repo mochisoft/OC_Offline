aefce8a15a548ecbc17ad5307e8b946d9150a284
Michael Ochieng <mochisoft@gmail.com>
2015-09-14 19:16:59 +0300

    Create rename_db.cmd

---
+++ rename_db.cmd
@@ -0,0 +1,4 @@
+REM Execute the python file which renames the database dump file to "study_name.backup" 
+REM as the zipped file comes with backup date extension
+REM - A shorter way could be to write this in dos
+"C:\path_to_python_file\renamedb.py"
